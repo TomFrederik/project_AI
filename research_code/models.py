2b8d122dad124a4da839301e944fd3ea9f8f6ff7
TomFrederik <tlieberum@outlook.de>
2021-07-08 21:07:18 +0200

    implemented decoder

--- research_code/models.py
+++ research_code/models.py
@@ -10,7 +10,7 @@ class MergeFramesWithBatch(nn.Module):
         super().__init__()
     
     def forward(self, input):
-        return input.view((input.shape[0] * input.shape[1], *input_shape[2:]))
+        return input.reshape((input.shape[0] * input.shape[1], *input.shape[2:]))
 
 class SplitFramesFromBatch(nn.Module):
     '''
@@ -21,7 +21,7 @@ class SplitFramesFromBatch(nn.Module):
         self.num_frames = num_frames
     
     def forward(self, input):
-        return input.view((-1, self.num_frames, *input_shape[2:]))
+        return input.reshape((-1, self.num_frames, *input.shape[1:]))
 
 class Transpose(nn.Module):
     '''
@@ -40,43 +40,70 @@ class ConvLSTM(nn.Module):
     def __init__(self, lstm_kwarg_list, num_frames):
         super().__init__()
         model_list = []
-        for kwargs in lstm_kwarg_list:
-            model_list.append(ConvLSTMCell(**kwargs)) # put through CLSTM cell
+        for i, kwargs in enumerate(lstm_kwarg_list):
+            # if img_shape is not provided, compute it automatically:
+            if 'img_shape' not in kwargs:
+                
+                try:
+                    last_img_shape = lstm_kwarg_list[i-1]['img_shape']
+                except:
+                    raise ValueError('No input img shape detected. Provide img shape for input to first layer')
+                
+                h_new = np.floor((last_img_shape[0] - lstm_kwarg_list[i-1]['kernel_size'])/3 + 1).astype(int)
+                kwargs['img_shape'] = (h_new, h_new)
+                print(f'No img_shape for layer {i+1} provided. Automatically computed img_shape: {(h_new, h_new)}')
+
+            # append modules
+            model_list.append(ConvLSTMCell(**kwargs, last_only=False)) # put through CLSTM cell
             model_list.append(MergeFramesWithBatch()) # merge frames with batch for BN
             model_list.append(nn.BatchNorm2d(num_features=kwargs['out_channels'])) # BatchNorm
-            model_list.append(nn.MaxPooling) # MaxPooling
+            model_list.append(nn.MaxPool2d(kernel_size=kwargs['kernel_size'])) # MaxPooling
             model_list.append(SplitFramesFromBatch(num_frames=num_frames)) # split frames from batch again for next iteration
         
-        self.net = nn.Sequential(**model_list)
+        self.net = nn.Sequential(*model_list)
+        
+        h_out = np.floor((lstm_kwarg_list[-1]['img_shape'][0] - lstm_kwarg_list[-1]['kernel_size'])/3 + 1).astype(int)
+        self.out_img_shape = (lstm_kwarg_list[-1]['out_channels'], h_out, h_out)
 
     def forward(self, input):
         return self.net(input)
 
 class ConvLSTMCell(nn.Module):
 
-    def __init__(self, in_channels, out_channels, kernel_size):
+    def __init__(self, in_channels, out_channels, kernel_size, img_shape, last_only=False):
         super().__init__()
 
-        #TODO: fill in shapes
-        self.conv_xi = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
-        self.conv_hi = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
-        self.W_ci = torch.zeros_like()
-
-        self.conv_xf = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
-        self.conv_hf = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
-        self.W_cf = torch.zeros_like()
-
-
-        self.conv_xc = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
-        self.conv_hc = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.last_only = last_only
+        self.img_shape = img_shape
         
-        self.conv_xo = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
-        self.conv_ho = nn.Conv2d(in_channels, out_channels, kernel_size, padding='same')
-        self.W_co = torch.zeros_like()
+        # define layers
+        # TODO Fix padding: this pytorch version doesn't seem to support same padding --> either update or compute explicitly
+        padding = (kernel_size - 1) // 2
+        
+        self.conv_xi = nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding)
+        self.conv_hi = nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding)
+        self.W_ci = nn.Parameter(data=torch.zeros(img_shape))
+
+        self.conv_xf = nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding)
+        self.conv_hf = nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding)
+        self.W_cf = nn.Parameter(data=torch.zeros(img_shape))
+
+
+        self.conv_xc = nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding)
+        self.conv_hc = nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding)
+        
+        self.conv_xo = nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding)
+        self.conv_ho = nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding)
+        self.W_co = nn.Parameter(data=torch.zeros(img_shape))
         
 
         # init weight vectors
-        # TODO
+        nn.init.kaiming_normal_(self.W_ci.data, nonlinearity='sigmoid')
+        nn.init.kaiming_normal_(self.W_cf.data, nonlinearity='sigmoid')
+        nn.init.kaiming_normal_(self.W_co.data, nonlinearity='sigmoid')
+        
             
     def forward(self, input):
         '''
@@ -88,72 +115,121 @@ class ConvLSTMCell(nn.Module):
         '''
         assert len(input.shape) == 5, f'input shape should have 5 entries, but is {input.shape}'
         
-        seq_len = input.shape[0]
-        h = torch.zeros_like(input)
-        c = torch.zeros_like(input)
+        # get sequence length T
+        seq_len = input.shape[1]
 
-        h_0 = torch.zeros((input.shape[0], *input.shape[2:]))
-        c_0 = torch.zeros((input.shape[0], *input.shape[2:]))
+        # init h and c
+        h = torch.zeros((*input.shape[:2], self.out_channels, *input.shape[3:]))
+        c = torch.zeros((*input.shape[:2], self.out_channels, *input.shape[3:]))
+
+        h_0 = torch.zeros((input.shape[0], self.out_channels, *input.shape[3:]))
+        c_0 = torch.zeros((input.shape[0], self.out_channels, *input.shape[3:]))
 
         for t in range(seq_len):
             x_t = input[:,t]
             
             if t == 0:
-                i_t = torch.sigmoid(self.conv_xi(x_t) + self.conv_hi(h_0) + torch.diag_embed(self.W_ci)(c_0))
-                f_t = torch.sigmoid(self.conv_xf(x_t) + self.conv_hf(h_0) + torch.diag_embed(self.W_cf)(c_0))
+                i_t = torch.sigmoid(self.conv_xi(x_t) + self.conv_hi(h_0) + self.W_ci * (c_0))
+                f_t = torch.sigmoid(self.conv_xf(x_t) + self.conv_hf(h_0) + self.W_cf * (c_0))
                 
                 c[:,t] = f_t * c_0 + i_t * torch.tanh(self.conv_xc(x_t) + self.conv_hc(h_0))
             
                 
             else:
-                i_t = torch.sigmoid(self.conv_xi(x_t) + self.conv_hi(h[:,t-1]) + torch.diag_embed(self.W_ci)(c[:,t-1]))
-                f_t = torch.sigmoid(self.conv_xf(x_t) + self.conv_hf(h[:,t-1]) + torch.diag_embed(self.W_cf)(c[:,t-1]))
+                i_t = torch.sigmoid(self.conv_xi(x_t) + self.conv_hi(h[:,t-1]) + self.W_ci * (c[:,t-1]))
+                f_t = torch.sigmoid(self.conv_xf(x_t) + self.conv_hf(h[:,t-1]) + self.W_cf * (c[:,t-1]))
                 
                 c[:,t] = f_t * c_0 + i_t * torch.tanh(self.conv_xc(x_t) + self.conv_hc(h[:,t-1]))
         
-            o_t = torch.sigmoid(self.conv_xo(x_t) + self.conv_ho(h[:,t-1]) + torch.diag_embed(self.W_co)(c[:,t]))
+            o_t = torch.sigmoid(self.conv_xo(x_t) + self.conv_ho(h[:,t-1]) + self.W_co) * (c[:,t])
             h[:,t] = o_t * torch.tanh(c[:,t])
         
-        # return c and h
-        return c, h
+        if self.last_only:
+            return h[:,-1]
+        else:
+            return h
 
-class CLSTM_Encoder(nn.Module):
+class CLSTMEncoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, lstm_kwarg_list, num_frames, latent_dim):
         super().__init__()
         #TODO
+
+        self.CLSTM = ConvLSTM(lstm_kwarg_list, num_frames)
+
+        self.flatten = nn.Flatten(start_dim=1, end_dim=-1)
+
+        self.clstm_out_shape = self.CLSTM.out_img_shape
+
+        self.linear = nn.Linear(np.prod(self.clstm_out_shape)*num_frames, 2*latent_dim)
     
-    def forward(self, x, y):
+    def forward(self, x):
         '''
         x - input, for mineRL (B, T, C, H, W) batch of stack of frames
-        y - conditioning input, for mineRL this is the (B, 64) obs tensor
         '''
-        out = self.net(x)
-        mean, log_std = torch.chunk(out, chunks=2, dim=-1)
+        clstm_out = self.CLSTM(x)
+        latent_out = self.linear(self.flatten(clstm_out))
+
+        mean, log_std = torch.chunk(latent_out, chunks=2, dim=-1)
         
         return mean, log_std
 
-class CLSTM_Decoder(nn.Module):
+class ConvDecoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, latent_dim, clstm_out_shape, num_channels, kernel_size):
+        '''
+        Args:
+            latent_dim - dimension of the latent space
+            clstm_out_shape - shape of an image after it is passed through the encoding CLSTM, should be of form (C, H, W)
+            num_channels - list of ints describing the number of channels after each convolution. 
+                           len(num_channels) is used to determine number of layers.
+        '''
         super().__init__()
-        #TODO
+        # MAYBE?
         # output needs to passed through 256-way softmax to encode for pixels
+        
+        self.clstm_out_shape = clstm_out_shape
+
+        # compute padding for same-padding
+        padding = (kernel_size - 1) // 2
+
+        # compute the upsample factor used in each layer
+        up_factor = np.exp(np.log(64 / clstm_out_shape[1]) / len(num_channels))
+        print(f'Upsample factor in ConvDecoder: {up_factor:.2f}')
+        img_sizes = [clstm_out_shape[1] * up_factor**i for i in range(1,len(num_channels))]
+        img_sizes.append(64)
+        img_sizes = np.floor(img_sizes).astype(int)
+        print(f'calculated image sizes during deconv: {img_sizes}')
+
+        self.linear = nn.Sequential(nn.Linear(latent_dim, np.prod(clstm_out_shape)), nn.GELU())
+        
+        num_channels = [clstm_out_shape[0]] + num_channels
+
+        layer_list = []
+        for i in range(len(num_channels)-1):
+            layer_list.append(nn.Upsample((img_sizes[i], img_sizes[i])))
+            layer_list.append(nn.Conv2d(num_channels[i], num_channels[i+1], kernel_size=kernel_size, padding=padding))
+
+        layer_list.append(nn.Conv2d(num_channels[-1], 3, kernel_size=kernel_size, padding=padding))
+        
+        self.deconv = nn.Sequential(*layer_list)
     
-    def forward(self, z, y):
+    def forward(self, z):
         '''
         z - latent vector
-        y - conditioning vector, for mineRL this is the (B, 64) obs tensor
         '''
-        #TODO
+        out = self.linear(z)
+        out = out.reshape((z.shape[0], *self.clstm_out_shape))
+        out = self.deconv(out)
 
 
-class CLSTM_cVAE(nn.Module):
+
+class CLSTMVAE(nn.Module):
 
     def __init__(self, encoder_kwargs, decoder_kwargs):
         super().__init__()
-        self.encoder = CLSTM_Encoder(**encoder_kwargs)
-        self.decoder = CLSTM_Decoder(**decoder_kwargs)
+        self.encoder = CLSTMEncoder(**encoder_kwargs)
+        self.decoder = ConvDecoder(**decoder_kwargs, clstm_out_shape=self.encoder.clstm_out_shape)
         #TODO
         self.CE_loss = nn.CrossEntropyLoss(reduction='none')
 
@@ -164,13 +240,12 @@ class CLSTM_cVAE(nn.Module):
         z = mean + torch.exp(log_std) * torch.normal(torch.zeros_like(mean), torch.ones_like(log_std))
         return z
     
-    def forward(self, x, y):
+    def forward(self, x):
         '''
         x - input, for mineRL (B, T, C, H, W) batch of stack of frames
-        y - conditioning vector, for mineRL this is the (B, 64) obs tensor
         '''
         # encode
-        mean, log_std = self.encoder(x, y)
+        mean, log_std = self.encoder(x)
 
         # compute KL difstance, i.e. regularization loss
         L_regul = (0.5 * (torch.exp(2 * log_std) + mean ** 2 - 1 - 2 * log_std)).sum(dim=-1)
@@ -179,7 +254,7 @@ class CLSTM_cVAE(nn.Module):
         z = self.sample(mean, log_std)
 
         # decode
-        x_new = self.decoder(z, y)
+        x_new = self.decoder(z)
         
         # convert x to classes
         x = x.uint8()
---
+++ research_code/test.py
@@ -0,0 +1,47 @@
+import models
+import torch
+import numpy as np
+
+inp = torch.rand((2,4,3,64,64))
+print(f'Input tensor shape: {inp.shape}')
+
+
+#############
+print('\n\nTesting ConvLSTM..')
+lstm_kwarg_list = [# first layer
+                    {'in_channels':3, 
+                    'out_channels':100, 
+                    'kernel_size':3, 
+                    'img_shape':(64,64)},
+                    # second layer
+                    {'in_channels':100, 
+                    'out_channels':50, 
+                    'kernel_size':3}]
+
+CLSTM = models.ConvLSTM(lstm_kwarg_list, num_frames=4)
+
+out = CLSTM(inp)
+print(f'Output tensor shape: {out.shape}')
+##############
+
+##############
+print('\n\nTesting CLSTMVAE..')
+encoder_kwargs = {
+                'lstm_kwarg_list':lstm_kwarg_list,
+                'num_frames':4,
+                'latent_dim':32
+                }
+decoder_kwargs = {
+                'num_channels':[50,100],
+                'latent_dim':32,
+                'kernel_size':3
+                }
+VAE = models.CLSTMVAE(encoder_kwargs, decoder_kwargs)
+out = VAE(inp)
+print(f'Output tensor shape: {out.shape}')
+##############
+
+
+##############
+
+##############
